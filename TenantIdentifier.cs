d8951b1946b01f796e9ca0cfccc8788e24b28ad4
Erick Osorio Sánchez <35073011+ErickOsorio@users.noreply.github.com>
2021-10-26 09:12:33 -0500

    Clase Middleware identificador de inquilino
    Se agrego clase TenantIdentifier que cambia el inquilino este es llamado en el startup del proyecto .Net, la clase UserRepository es ejemplo de como se utiliza la nueva conexión del inquilino

---
+++ TenantIdentifier.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System.Linq;
+using System.Threading.Tasks;
+using TentiaCloud.UtilitiesAPI.Entities;
+using TentiaCloud.UtilitiesAPI.Models.Db;
+
+namespace TentiaCloud.UtilitiesAPI.Middleware
+{
+    public class TenantIdentifier
+    {
+        private readonly RequestDelegate _next;
+        public IConfiguration _configuration { get; }
+
+        public TenantIdentifier(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext httpContext, GlobalDBContext dbContext)
+        {
+            var tenantID = httpContext.Request.Headers["tenantID"].FirstOrDefault();
+            if (!string.IsNullOrEmpty(tenantID))
+            {
+                var tenant = dbContext.customer.FirstOrDefault(p => p.instancia == tenantID);
+
+                var connectionStringBuilder = new MySqlConnectionStringBuilder
+                {
+                    Server = tenant.nombre_servidor,
+                    UserID = tenant.usuario_db,
+                    Password = tenant.pwd_db,
+                    Database = tenant.nombre_db
+                };
+
+                Tenant connectionTenant = new()
+                {
+                    Id = tenant.id,
+                    Nombre = tenant.instancia,
+                    CadenaConexion = connectionStringBuilder.ConnectionString
+                };
+                httpContext.Items["TENANT"] = connectionTenant;
+            }
+
+            await _next.Invoke(httpContext);
+        }
+    }
+
+    public static class TenantIdentifierExtension
+    {
+        public static IApplicationBuilder UseTenantIdentifier(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<TenantIdentifier>();
+            return app;
+        }
+    }
+}
---
+++ UsersRepository.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using TentiaCloud.UtilitiesAPI.Entities;
+using TentiaCloud.UtilitiesAPI.Models.Db;
+using TentiaCloud.UtilitiesAPI.Repository.IRepository;
+
+namespace TentiaCloud.UtilitiesAPI.Repository
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly GlobalDBContext _context;
+        private readonly Tenant _connection;
+        private readonly IConfiguration _configuration;
+
+        public UsersRepository(GlobalDBContext context, IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+
+            if (httpContextAccessor.HttpContext != null)
+            {
+                _connection = (Tenant)httpContextAccessor.HttpContext.Items["TENANT"];
+            }
+        }
+
+        public ICollection<Users> GetAllUsers()
+        {
+            // Obtenemos los datos del usuario
+            MySqlConnection conn = new(_connection.CadenaConexion);
+            MySqlCommand Cmd = new();
+            MySqlDataReader reader = null;
+            Cmd.CommandType = CommandType.Text;
+            Cmd.CommandText = "SELECT idadmin, apellidos, nombreusuario, idperfil, idempleado, idSuc FROM administracion_usuarios";
+            Cmd.Connection = conn;
+
+            conn.Open();
+            reader = Cmd.ExecuteReader();
+
+            Users user = null;
+            List<Users> listUser = new();
+            while (reader.Read())
+            {
+                user = new Users
+                {
+                    idadmin = Convert.ToInt32(reader.GetValue(0)),
+                    apellidos = reader.GetValue(1).ToString(),
+                    nombreusuario = reader.GetValue(2).ToString(),
+                    idperfil = Convert.ToInt32(reader.GetValue(3)),
+                    idempleado = Convert.ToInt32(reader.GetValue(4)),
+                    sucursal = reader.GetValue(5).ToString()
+                };
+                listUser.Add(user);
+            }
+            conn.Close();
+            return listUser;
+        }
+
+        public ICollection<Users> GetUserById(int Id)
+        {
+
+            // Obtenemos los datos del usuario
+            MySqlConnection conn = new(_connection.CadenaConexion);
+            MySqlDataReader reader = null;
+            MySqlCommand Cmd = new MySqlCommand();
+            Cmd.CommandType = CommandType.Text;
+            Cmd.CommandText = "SELECT au.idadmin, au.apellidos, au.nombreusuario, au.idperfil, au.idempleado, ms.nombre as sucursal FROM administracion_usuarios au, mrp_sucursal ms where au.idSuc = ms.idSuc and au.idadmin = '" + Id + "'";
+            Cmd.Connection = conn;
+
+
+            conn.Open();
+            reader = Cmd.ExecuteReader();
+
+            Users user = null;
+            List<Users> listUser = new();
+            while (reader.Read())
+            {
+                user = new Users
+                {
+                    idadmin = Convert.ToInt32(reader.GetValue(0)),
+                    apellidos = reader.GetValue(1).ToString(),
+                    nombreusuario = reader.GetValue(2).ToString(),
+                    idperfil = Convert.ToInt32(reader.GetValue(3)),
+                    idempleado = Convert.ToInt32(reader.GetValue(4)),
+                    sucursal = reader.GetValue(5).ToString()
+                };
+
+                listUser.Add(user);
+            }
+            conn.Close();
+
+            return listUser;
+        }
+    }
+}
